93b0b7f830a56ac9347c95a9136d69f286fe5c15
dammy-gif826 <darmianjimz@gmail.com>
2025-02-28 21:47:45 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Jimoh', // Your name
+  ownerNumber: '08020834293', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
